04cf5a4586db538b1ec661c0013b329b765e1d36
rhodosaur <hai@rsaur.me>
2020-10-27 03:00:54 +0100

    refresh

--- run-netcore.ps1
+++ run-netcore.ps1
@@ -1,6 +1,10 @@
 $exe = Join-Path $PSScriptRoot "RawBencher.Core\bin\Release\netcoreapp3.1\RawBencher.Core.exe"
 $path =  Join-Path $PSScriptRoot ("Results\{0:yyyyMMdd_HHmmss}_jc_netcore31.txt" -f (Get-Date))
 
+Write-Host "Building project..." -ForegroundColor Yellow
+dotnet build --nologo -v q -c Release
+if ($LastExitCode -ne 0) { Exit -1 }
+
 Write-Host "Benchmarking .NET Core 3.1..." -ForegroundColor Yellow
 & $exe /a > $path
 $dest = Split-Path $path -Leaf
--- run-netfx.ps1
+++ run-netfx.ps1
@@ -1,6 +1,10 @@
 $exe = Join-Path $PSScriptRoot "RawBencher\bin\Release\RawBencher.exe"
 $path =  Join-Path $PSScriptRoot ("Results\{0:yyyyMMdd_HHmmss}_jc_netfx.txt" -f (Get-Date))
 
+Write-Host "Building project..." -ForegroundColor Yellow
+dotnet build --nologo -v q -c Release
+if ($LastExitCode -ne 0) { Exit -1 }
+
 Write-Host "Benchmarking .NET Framework..." -ForegroundColor Yellow
 & $exe /a > $path
 $dest = Split-Path $path -Leaf
